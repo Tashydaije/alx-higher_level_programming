308f5a798f3dd61475ae24fb20540d3c340c900b
Tashydaije <mbuguanatasha01@gmail.com>
2023-11-20 14:05:19 -0800

    task0: prints x elements of a list

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+
+    try:
+        for item in my_list:
+            if (count < x):
+                print(item, end="")
+                count += 1
+            else:
+                break
+        print()
+        return count
+    except IndexError:
+        return count
