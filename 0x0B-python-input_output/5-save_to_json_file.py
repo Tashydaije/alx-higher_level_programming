7842a7ce79f005da90859e2e72e95b3204e9f67d
Tashydaije <mbuguanatasha01@gmail.com>
2023-12-05 16:41:28 -0800

    task5: writes obj to text file

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""Module for writing object to text file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """ Writes obj to text file using json repn
+
+        Args:
+            my_obj: an object
+            filename (str): file path
+
+        Returns:
+            None
+    """
+
+    with open(filename, mode="w", encoding="utf-8") as file:
+        obj_json = json.dumps(my_obj)
+    return file.write(obj_json)
