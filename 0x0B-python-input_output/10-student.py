63c85d4f08b5a58b1fe947d2cd7883354f46962b
Tashydaije <mbuguanatasha01@gmail.com>
2023-12-06 07:27:02 -0800

    task10: student class to json with filter

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+""" Defines a class Student """
+
+
+class Student:
+    """ Represents a student """
+
+    def __init__(self, first_name, last_name, age):
+        """ Initialization of the instance variables
+
+            Args:
+                first_name (str): students first name
+                last_name (str): students last name
+                age (int): students age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieves a dictionary representation of a Student instance
+
+        if attrs is a list of strings, only attr names in list be retrieved
+        Otherwise, all attributes must be retrieved
+
+            Args:
+                attrs (list): a list of strings
+        """
+        if type(attrs) is list and \
+        all(type(element) is str for element in attrs):
+            return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
+            print("Wamlambez {}", k)
+        return self.__dict__
