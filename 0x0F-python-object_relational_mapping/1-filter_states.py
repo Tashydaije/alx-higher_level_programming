79c156c11924870d9193b273a47c064246a5bff1
Tashydaije <mbuguanatasha01@gmail.com>
2024-02-23 15:29:25 -0800

    filter db results for states starting with N

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+
+"""Module for accessing hbtn_0e_0_usa db"""
+import MySQLdb
+import sys
+
+
+def dbConnect(host, user, password, database, port):
+    """Connects to db
+        Args:
+            host (str): The 'db host'
+            user (str): The 'db user'
+            password (str): The 'db user password'
+            database (str): The 'db name'
+            port (str): The 'db port' to connect to
+        Returns: The db connection object
+    """
+
+    return MySQLdb.connect(host, user, password, db=database, port=port)
+
+
+def main():
+    """Entry point of the application"""
+    args = sys.argv[1:]
+    db_host = 'localhost'
+    db_user = args[0]
+    db_password = args[1]
+    database = args[2]
+    db_port = 3306
+    query = """
+    SELECT * FROM states WHERE states.name LIKE 'N%' ORDER BY states.id ASC;
+    """
+    db_connection = dbConnect(
+        host=db_host,
+        user=db_user,
+        password=db_password,
+        database=database,
+        port=db_port)
+    db_cursor = db_connection.cursor()
+    db_cursor.execute(query)
+    rows = db_cursor.fetchall()
+    for row in rows:
+        print(row)
+
+
+if __name__ == "__main__":
+    main()
